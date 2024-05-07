eed95b2fe5fb4cba541f35c635d91955fe27e4c3
D4n13l-D33 <abrahamdanielu793@gmail.com>
2024-05-07 10:08:43 +0100

    funding

--- src/funding.sol
+++ src/funding.sol
@@ -5,12 +5,19 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 // import "./StudentFundingTkn.sol";
 
 contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
     ERC20 private _nativeToken;
     uint256 public proposalCount;
     uint256 public proposalDeadline;
+
+    struct Student {
+        address student;
+        string imageIPFSHash;
+        uint256 disbursedBalance;
+    }
 
     struct Proposal {
         uint256 proposalId;
@@ -19,10 +26,8 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
         string title;
         string referral;
         ProposalStatus status;
-        string imageIPFSHash;
         string imageIPFSHashTranscript;
         uint256 amount;
-        bool isRegistered;
         bool approved;
         bool disbursed;
     }
@@ -35,8 +40,10 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
         Executed
     }
 
-    Proposal[] public proposals;
-    mapping (address => Proposal) public proposalByAddress;
+    Proposal[] public pendingProposals;
+    Proposal[] public approvedProposals;
+    mapping(address => Student) public registeredStudents;
+    mapping(address => Proposal) public proposalByAddress;
     mapping(address => bool) public isStudent;
     mapping(address => bool) public revoked;
     mapping(address => uint256) public studentMap;
@@ -73,10 +80,7 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
     }
 
     modifier onlyOwnerOrEscrow() {
-        require(
-            msg.sender == owner() || msg.sender == address(this),
-            "Not authorized"
-        );
+        require(msg.sender == owner(), "Not authorized");
         _;
     }
 
@@ -86,86 +90,94 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
 
     receive() external payable {}
 
-    function registerStudent( string memory _imageIPFSHash) external {
+    function registerStudent(string memory _imageIPFSHash) external {
         require(!isStudent[msg.sender], "Student is already registered");
         require(
             bytes(_imageIPFSHash).length > 0,
             "Image IPFS hash must not be empty"
         );
         isStudent[msg.sender] = true;
-        Proposal storage proposal = proposalByAddress[msg.sender];
-        proposal.imageIPFSHashTranscript = _imageIPFSHash;
-        proposal.student = msg.sender;
+        Student storage student = registeredStudents[msg.sender];
+        student.student = msg.sender;
+        student.imageIPFSHash = _imageIPFSHash;
 
         emit StudentRegistered(msg.sender, _imageIPFSHash);
     }
 
     function createProposal(
-         string memory _title,
+        string memory _title,
         string memory _description,
-         uint256 _amount,
+        uint256 _amount,
         string memory _imageIPFSHash,
-         string memory _referral       
-    ) external validStudent returns(uint256 id) {
+        string memory _referral
+    ) external validStudent returns (uint256 id) {
         require(!revoked[msg.sender], "You are revoked due to offence");
         Proposal storage proposal = proposalByAddress[msg.sender];
         proposal.student = msg.sender;
         proposal.proposalId = proposalCount;
         proposal.description = _description;
+        proposal.imageIPFSHashTranscript = _imageIPFSHash;
         proposal.status = ProposalStatus.Pending;
-        proposal.imageIPFSHash = _imageIPFSHash;
         proposal.amount = _amount;
         proposal.title = _title;
         proposal.referral = _referral;
 
-        proposals.push(proposal);
+        pendingProposals.push(proposal);
 
         emit ProposalCreated(proposalCount, _description, msg.sender);
-        proposalCount+=1;
+        proposalCount += 1;
         id = proposal.proposalId;
-        
     }
 
     function approveProposal(uint256 _proposalId) external onlyOwner {
-        Proposal storage proposal = proposals[_proposalId];
+        Proposal storage proposal = pendingProposals[_proposalId];
         require(
             proposal.status == ProposalStatus.Pending,
             "Proposal not pending"
         );
         proposal.status = ProposalStatus.Approved;
+        approvedProposals.push(proposal);
+
         emit Status(_proposalId, ProposalStatus.Approved);
     }
 
-    function getProposals() external view returns(Proposal[] memory){
-        return  proposals;
+    function getApprovedProposals() external view returns (Proposal[] memory) {
+        return approvedProposals;
     }
 
-    function closeProposal(uint256 _proposalId) external onlyOwner {
-        Proposal storage proposal = proposals[_proposalId];
-        require(
-            proposal.status == ProposalStatus.Approved ||
-                proposal.status == ProposalStatus.Pending,
-            "Invalid proposal status"
-        );
+    function getPendingProposals() external view returns (Proposal[] memory) {
+        return pendingProposals;
+    }
+
+    function closeProposal(uint256 _proposalId) private {
+        Proposal storage proposal = approvedProposals[_proposalId];
         proposal.status = ProposalStatus.Closed;
         emit ProposalClosed(_proposalId, ProposalStatus.Closed);
     }
 
     function donate(uint256 _amount, uint256 _proposalId) external payable {
-        require(_amount> 0, "Amount must be greater than zero");
-        require(_nativeToken.balanceOf(msg.sender)>= _amount, "Insufficient balance");
-        uint256 usdtAmount = _amount*10**18;
+        require(_amount > 0, "Amount must be greater than zero");
+        require(
+            approvedProposals[_proposalId].amount >
+                studentMap[approvedProposals[_proposalId].student],
+            "Proposal Amount Reached, Thank you!. Please Consider other Proposals"
+        );
+        require(
+            _nativeToken.balanceOf(msg.sender) >= _amount,
+            "Insufficient balance"
+        );
+        uint256 usdtAmount = _amount;
 
         //Transfer USDT tokens from the sender to this contract
-         require(
-             _nativeToken.transferFrom(msg.sender, address(this), usdtAmount),
-             "Token transfer failed"
-         );
+        require(
+            _nativeToken.transferFrom(msg.sender, address(this), usdtAmount),
+            "Token transfer failed"
+        );
 
         // Update student's balance
-        studentMap[proposals[_proposalId].student] += usdtAmount;
+        studentMap[approvedProposals[_proposalId].student] += usdtAmount;
 
-        emit Donation(msg.sender,usdtAmount, block.timestamp);
+        emit Donation(msg.sender, usdtAmount, block.timestamp);
     }
 
     function withdraw(uint256 _amount) external validStudent {
@@ -178,32 +190,31 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
         Proposal storage proposal = proposalByAddress[msg.sender];
         //withdraw
         studentMap[proposal.student] -= _amount;
-        _nativeToken.transferFrom(address(this),msg.sender, _amount);
-        
+        _nativeToken.transferFrom(address(this), msg.sender, _amount);
     }
 
     function disburseFund(uint256 _proposalId) external nonReentrant onlyOwner {
-        require(_proposalId < proposals.length, "Invalid proposal ID");
+        require(_proposalId < approvedProposals.length, "Invalid proposal ID");
         require(
-            proposals[_proposalId].status == ProposalStatus.Approved,
+            approvedProposals[_proposalId].status == ProposalStatus.Approved,
             "Proposal not approved"
         );
-        require(!proposals[_proposalId].disbursed, "Loan already disbursed");
-
-        proposals[_proposalId].disbursed = true;
         require(
-            _nativeToken.balanceOf(address(this)) >=
-                proposals[_proposalId].amount,
-            "Insufficient contract balance"
+            !approvedProposals[_proposalId].disbursed,
+            "Loan already disbursed"
         );
-        proposals[_proposalId].amount -=  proposals[_proposalId].amount;
-        _nativeToken.transfer(
-            proposals[_proposalId].student,
-            proposals[_proposalId].amount
-        );
+        require(studentMap[approvedProposals[_proposalId].student] >= approvedProposals[_proposalId].amount, "Amount not reached yet");
+        
+        approvedProposals[_proposalId].disbursed = true;
+
+        closeProposal(_proposalId);
+        
+        registeredStudents[approvedProposals[_proposalId].student].disbursedBalance += approvedProposals[_proposalId].amount;
+
+
         emit Donation(
-            proposals[_proposalId].student,
-            proposals[_proposalId].amount,
+            approvedProposals[_proposalId].student,
+            approvedProposals[_proposalId].amount,
             block.timestamp
         );
     }
@@ -212,8 +223,8 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
         uint256 _proposalId,
         bool _proposalState
     ) external onlyOwner {
-        require(_proposalId < proposals.length, "Invalid proposal ID");
-        Proposal storage proposal = proposals[_proposalId];
+        require(_proposalId < pendingProposals.length, "Invalid proposal ID");
+        Proposal storage proposal = pendingProposals[_proposalId];
         require(
             proposal.status == ProposalStatus.Pending,
             "Proposal status not pending"
@@ -241,19 +252,12 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
         proposalDeadline = _proposalDeadline;
     }
 
-    function getProposal(
-        uint256 _proposalId
-    ) external view returns (Proposal memory) {
-        require(_proposalId < proposals.length, "Invalid proposal ID");
-        return proposals[_proposalId];
-    }
-
-    function getProposalStatus(
-        uint256 _proposalId
-    ) external view returns (ProposalStatus) {
-        require(_proposalId < proposals.length, "Invalid proposal ID");
-        return proposals[_proposalId].status;
-    }
+    // function getProposalStatus(
+    //     uint256 _proposalId
+    // ) external view returns (ProposalStatus) {
+    //     require(_proposalId < proposals.length, "Invalid proposal ID");
+    //     return proposals[_proposalId].status;
+    // }
 
     function pauseContract() external onlyOwner {
         super._pause();
@@ -284,7 +288,7 @@ contract FundingStudent is Pausable, ReentrancyGuard, Ownable {
 //         ERC20("MyToken", "MTK")
 //         Ownable(msg.sender)
 //     { _mint(msg.sender, 1000000000000000000000000000);
-    
+
 //     }
 
 // }
